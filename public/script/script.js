33610dff13a84d846c2d269bb4be606a68a7dd10
Thiago Loschi <thiagoloschi@gmail.com>
2017-05-16 00:57:41 -0300

    Small updates

--- public/script/script.js
+++ public/script/script.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
-    alert($( window ).width());
+    swal("Sorry for the inconvenience!", "This website is being updated.", "warning");
 
-        $('#fullpage').fullpage();
-    
+    //alert($( window ).width());
+
+    $('#fullpage').fullpage();
 
 	$('.text1').funnyText({
 		speed: 550,
@@ -31,5 +32,34 @@ $(document).ready(function() {
         activeColor: '#2c2c2c',
         color: '#00AEEF',
         direction: 'up'
+    });
+
+    // $('form').submit(){
+    //     swal({
+    //       title: "Are you done, already?"
+    //       text: "Please take your time!",
+    //       type: "question",
+    //       showCancelButton: true,
+    //       closeOnConfirm: false,
+    //       showLoaderOnConfirm: true,
+    //     },
+    //     function(){
+    //
+    //     });
+    // }
+
+    $('#send').click(function(event){
+        event.preventDefault();
+        $.post("https://formspree.io/thiagoloschi@gmail.com",
+        {
+          name: $('#name').text(),
+          _replyto: $('#_replyto').text(),
+          message: $('#message').text()
+        },
+        function(data){
+            alert('foi', data);
+        },function(data) {
+            alert('erro', data)
+        });
     });
 });
